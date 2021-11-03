fdf2d38d590a4f681fe06a730fa9614b05d2426a
BAR-KEY <park96h@naver.com>
2021-11-03 15:26:31 +0900

    create react projcet

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import './logo.svg';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <h2>Let's develop management system!</h2>
+    </div>
+  );
+}
+
+export default App;
